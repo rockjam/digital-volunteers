6de49ff8dae6ef8d446a0e8e3eb240751cf80367
Nikolai Tatarinov <5min4eq.unity@gmail.com>
2022-04-16 11:11:19 +0200

    keep only Arrival centres and AnkER facilities

--- src/dv/bamf.clj
+++ src/dv/bamf.clj
@@ -1,7 +1,7 @@
 (ns dv.bamf
   (:require
    [clojure.string :as str]
-   [reaver :refer [extract-from text attrs]]))
+   [reaver :refer [extract-from text attr attrs]]))
 
 (def base-url "https://www.bamf.de")
 
@@ -12,14 +12,15 @@
   (extract-from
    source
    ".c-locations__content > .js-locations-content > .c-teaser.c-teaser--row"
-   [:title :address :geo :contact-link]
+   [:title :address :geo :contact-link :type]
    ".c-teaser__text.c-teaser__text--column > .c-teaser__heading > span:eq(0)" text
    ".c-teaser__text.c-teaser__text--column > .address span" text
    ".c-teaser.c-teaser--row" (fn [x] (let [attrs (attrs x)]
                                        {:latitude  (:data-geo-lat attrs)
                                         :longitude (:data-geo-long attrs)}))
-   ".c-teaser__text.c-teaser__text--column .c-link" #(let [link (reaver/attr % :href)]
-                                                       (when link (sanitize-link (str base-url "/" (reaver/attr % :href)))))))
+   ".c-teaser__text.c-teaser__text--column .c-link" #(let [link (attr % :href)]
+                                                       (when link (sanitize-link (str base-url "/" (attr % :href)))))
+   ".c-teaser.c-teaser--row" (attr :data-geo-marker-type)))
 
 (defn to-csv [list]
   (str/join "," (map #(str "\"" % "\"") list)))
@@ -34,10 +35,15 @@
            (get-in location [:geo :latitude])
            (:contact-link location)]))
 
+(defn filter-location [location]
+  (or (= (:type location) "aussenstelle-im-ankunftszentrum") (= (:type location) "aussenstelle-in-ankereinrichtung")))
+
 (defn -main []
   (let [html (slurp "https://www.bamf.de/EN/Behoerde/Aufbau/Standorte/standorte-node.html")
         source (reaver/parse html)
-        locations (extract-locations source)
+        locations (->> source
+                       (extract-locations)
+                       (filter filter-location))
 
         results (str/join "\n" (concat [csv-header] (map location-to-csv locations)))]
     (println "locations: " locations)
